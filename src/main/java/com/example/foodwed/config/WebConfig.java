a974153678986977d444f7355bb303c2f9a34653
namblu <vunam3445@gmail.com>
2024-11-07 23:36:01 +0700

    Add config files in foodwed project

---
+++ src/main/java/com/example/foodwed/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.example.foodwed.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")  // Cho phép tất cả các endpoint
+                .allowedOrigins("http://localhost:3000") // Cho phép từ origin React
+                .allowedMethods("GET", "POST", "PUT", "DELETE") // Các phương thức HTTP được phép
+                .allowedHeaders("*") // Cho phép tất cả các headers
+                .allowCredentials(true); // Cho phép gửi cookie và thông tin xác thực
+    }
+}
