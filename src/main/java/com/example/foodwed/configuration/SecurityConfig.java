d1b527170418f2aae2498edf7fdc941d87d43398
QuyBX07 <duyquybaixa@gmail.com>
2024-12-10 23:13:51 +0700

    '

--- src/main/java/com/example/foodwed/configuration/SecurityConfig.java
+++ src/main/java/com/example/foodwed/configuration/SecurityConfig.java
@@ -28,11 +28,12 @@ import java.util.List;
 @EnableWebSecurity
 public class SecurityConfig {
     private final String[] PUBLIC_ENDPOINS_POST = {"/auth/signup", "/auth/token", "/auth/introspect","/auth/google"};
-    private final String[] PUBLIC_ENDPOINS_GET = {"/category","/suggestion/**","foodwed/images/**"};
+    private final String[] PUBLIC_ENDPOINS_GET = {"/category","/suggestion/**","foodwed/images/**","/recipe/recipeAll"};
     private final String[] ADMIN_AUTHEN_GET = {"foodwed/recipe"};
     private final String[] ADMIN_AUTHEN_POST = {"/foodwed/recipe/create","/foodwed/category/create"};
     private final String[] ADMIN_AUTHEN_PUT = {"/foodwed/recipe/update","/foodwed/category/update"};
     private final String[] ADMIN_AUTHEN_DELETE = {"/foodwed/recipe/delete","/foodwed/category/delete"};
+    private final String[] USER_AUTHEN_GET = {"/foodwed/order/uorder/**"};
     private String signerKey = "GtuAkpoXNfZOhcfdgkDJQ+N1Pd1pDwlc0syKYXZPQJT2ZI+mlWkd8Go5XL6rz93j";
 
     @Bean
